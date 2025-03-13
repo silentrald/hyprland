de494bf46e4eb193103636cdf198e312747695cd
silentrald <silentrald1216@gmail.com>
2025-03-13 22:48:57 +0800

    added color theme to setup

---
+++ rofi.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+
+git clone https://github.com/adi1090x/rofi
+cd rofi
+
+# Setup the color theme
+sed -i "s/@import \"~\/.config\/rofi\/colors\/onedark.rasi\"/@import \"~\/.config\/rofi\/colors\/custom.rasi\"" ./files/launchers/**/*.*
+
+echo "* {
+    background:     #222222FF;
+    background-alt: #111111FF;
+    foreground:     #FF99CCFF;
+    selected:       #FF99CCFF;
+    active:         #FFFFFFFF;
+    urgent:         #FFC0D3FF;
+}" > ./files/colors/custom.rasi
+
+./setup.sh
+
---
+++ setup.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+sudo ln -s "$PWD/hypr/" ~/.config/hypr
+sudo ln -s "$PWD/mako/" ~/.config/mako
+# sudo ln -s "$PWD/qt6ct/" ~/.config/qt6ct
+sudo ln -s "$PWD/waybar/" ~/.config/waybar
+sudo ln -s "$PWD/wlogout/" ~/.config/wlogout
+sudo ln -s "$PWD/hypr-shared/" ~/.config/hypr-shared
+
