791797ba7123e2160994a5b6780567876cdc45f0
silentrald <silentrald1216@gmail.com>
2025-03-14 00:21:20 +0800

    added uwsm in installation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S hyprland hypridle hyprlock hyprpaper waybar rofi && \
+sudo pacman -S hyprland hypridle hyprlock hyprpaper waybar rofi uwsm && \
 paru -S brightnessctl-git mako wlogout
