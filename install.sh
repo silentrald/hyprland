f7d17c337a23c942eddc676c67e0b6b24e8c258e
silentrald <silentrald1216@gmail.com>
2025-04-14 22:26:27 +0800

    fixed some cofiguration issues

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S hyprland hypridle hyprlock hyprpaper waybar rofi uwsm && \
+sudo pacman -S hyprland hypridle hyprlock hyprpaper waybar rofi uwsm grim slurp && \
 paru -S brightnessctl-git mako wlogout
