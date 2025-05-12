bdec59a22d261f03852375bcaf426dde45368b4d
silentrald <silentrald1216@gmail.com>
2025-05-12 10:44:48 +0800

    chore: added kitty config

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ sudo ln -s "$PWD/hypr/" ~/.config/hypr && \
 sudo ln -s "$PWD/mako/" ~/.config/mako && \
 sudo ln -s "$PWD/waybar/" ~/.config/waybar && \
 sudo ln -s "$PWD/wlogout/" ~/.config/wlogout && \
-sudo ln -s "$PWD/hypr-shared/" ~/.config/hypr-shared
+sudo ln -s "$PWD/hypr-shared/" ~/.config/hypr-shared && \
+sudo ln -s "$PWD/kitty/" ~/.config/kitty
 
