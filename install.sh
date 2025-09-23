0909fe6064259a3de7e91140de56c9199d2ec6f1
silentrald <silentrald1216@gmail.com>
2025-09-23 11:22:11 +0800

    chore: added kitty in install and remove gestures in hyprland

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S hyprland hypridle hyprlock hyprpaper waybar rofi uwsm grim slurp && \
+sudo pacman -S kitty hyprland hypridle hyprlock hyprpaper waybar rofi uwsm grim slurp && \
 paru -S brightnessctl-git mako wlogout
