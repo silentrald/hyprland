9098c70f108384c99845329f755a9f8ebc866d30
silentrald <silentrald1216@gmail.com>
2025-09-23 11:40:18 +0800

    chore: fix rofi installation

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sudo pacman -S kitty hyprland hypridle hyprlock hyprpaper waybar rofi uwsm grim slurp && \
+echo "Installing pacman packages"
+sudo pacman -S kitty hyprland hypridle hyprlock hyprpaper waybar rofi uwsm grim slurp
+
+echo "Installing paru packages"
 paru -S brightnessctl-git mako wlogout
+
+echo "Installing source packages"
+./rofi.sh
+
--- rofi.sh
+++ rofi.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo "Installing rofi"
 git clone https://github.com/adi1090x/rofi
 cd rofi
 
 # Setup the color theme
-sed -i 's/\@import "~\/.config\/rofi\/colors\/onedark.rasi"/\@import "~\/.config\/rofi\/colors\/custom.rasi"/g' ./files/launchers/**/*.*
+sed -i 's/\@import "~\/.config\/rofi\/colors\/onedark.rasi"/\@import "~\/.config\/rofi\/colors\/custom.rasi"/g' ./files/launchers/**/shared/colors.rasi
 
 echo "* {
     background:     #222222FF;
