ea8ec1c0bc4b59c7185113de65ec59f7d4bb22d0
silentrald <silentrald1216@gmail.com>
2026-09-10 13:59:34 +0800

    chore: use updated waybar-git version

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Installing pacman packages"
-sudo pacman -S kitty hyprland hypridle hyprlock hyprpaper waybar rofi uwsm grim slurp
+sudo pacman -S kitty hyprland hypridle hyprlock hyprpaper rofi uwsm grim slurp
 
 echo "Installing paru packages"
-paru -S brightnessctl-git mako wlogout librewolf
+paru -S brightnessctl-git mako wlogout librewolf waybar-git
 
 echo "Installing source packages"
 ./rofi.sh
